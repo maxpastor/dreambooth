63d31c6355a51d95a607dd900cc38096098035d6
Maxence Pastor <23473052+maxpastor@users.noreply.github.com>
2022-11-01 14:52:10 +0000

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,10 @@
+from diffusers import StableDiffusionPipeline
+import torch
+
+model_id = "./output"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
+
+prompt = "A photo of sks dog in a bucket"
+image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+
+image.save("dog-bucket.png")
