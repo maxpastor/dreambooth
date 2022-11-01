c8ba63a69a526229e922fe3ca44c6f04ce7f66fe
Maxence Pastor <23473052+maxpastor@users.noreply.github.com>
2022-11-01 14:59:00 +0000

    Update inference.py

--- inference.py
+++ inference.py
@@ -4,7 +4,8 @@ import torch
 model_id = "./output"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
 
-prompt = "A photo of sks dog in a bucket"
-image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+for i in range(50):
+    prompt = "A photo of sks dog in a bucket"
+    image = pipe(prompt, num_inference_steps=200, guidance_scale=7.5).images[0]
 
-image.save("dog-bucket.png")
+    image.save("./dogs/dog-bucket-"+str(i)+".png")
