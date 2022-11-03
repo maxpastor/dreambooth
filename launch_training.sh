889e538a8ab4dd286e87101777baf1c0272e0ce5
Maxence Pastor <23473052+maxpastor@users.noreply.github.com>
2022-11-03 09:30:55 +0000

    Create launch_training.sh

---
+++ launch_training.sh
@@ -0,0 +1,18 @@
+accelerate launch --num_cpu_threads_per_process 24 train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --instance_data_dir=$INSTANCE_DIR \
+  --class_data_dir=$CLASS_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --with_prior_preservation --prior_loss_weight=1.0 \
+  --instance_prompt="a photo of sks dog" \
+  --class_prompt="a photo of a dog" \
+  --resolution=300 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 --gradient_checkpointing \
+  --use_8bit_adam \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --num_class_images=300 \
+  --max_train_steps=800 \
+  --hub_token="<your-hugginface-token>" \
